9e3d8455502c257f65cdf051fae67107bcb988ca
venka <venka@192.168.0.42>
2021-01-18 00:33:48 -0800

    initial checkin for deck of cards api test cases

---
+++ deckofcards.bat
@@ -0,0 +1,9 @@
+set C:\Users\venkat\eclipse-workspace\DeckofCards
+
+cd %projectLocation%
+
+set classpath=C:\Users\venkat\eclipse-workspace\DeckofCards\bin;C:\Users\venka\eclipse-workspace\DeckofCards\lib\*
+
+java org.testng.TestNG C:\Users\venkat\eclipse-workspace\DeckofCards\testng.xml
+
+pause
